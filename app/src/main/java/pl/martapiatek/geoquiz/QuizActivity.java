d51eda17e6cd02843e5bc0968031a355fca28ae1
MartaPiatek <martusia.piatek@gmail.com>
2018-01-06 16:02:38 +0100

    Dodanie listenera do TextView

--- app/src/main/java/pl/martapiatek/geoquiz/QuizActivity.java
+++ app/src/main/java/pl/martapiatek/geoquiz/QuizActivity.java
@@ -35,6 +35,7 @@ public class QuizActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
                 updateQuestion();
             }
         });
