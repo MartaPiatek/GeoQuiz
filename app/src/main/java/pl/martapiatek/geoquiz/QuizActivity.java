a771c276f65a338d06938ac0ad4a7ab7417e83ff
MartaPiatek <martusia.piatek@gmail.com>
2018-01-06 21:18:39 +0100

    Dodanie układu dla poziomej orientacji ekranu

--- app/src/main/java/pl/martapiatek/geoquiz/QuizActivity.java
+++ app/src/main/java/pl/martapiatek/geoquiz/QuizActivity.java
@@ -2,17 +2,25 @@ package pl.martapiatek.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
+import android.view.ScaleGestureDetector;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import javax.crypto.KeyAgreement;
+
 public class QuizActivity extends AppCompatActivity {
 
+    public static final String KEY_INDEX = "index";
+    public static final String KEY_POINTS = "points";
     private Button mTrueButton;
     private Button mFalseButton;
-    private Button mNextBtton;
+    private ImageButton mNextButton;
+    private ImageButton mPreviousButton;
     private TextView mQuestionTextView;
 
     private Question[] mQuestionBank = new Question[]{
@@ -24,11 +32,25 @@ public class QuizActivity extends AppCompatActivity {
             new Question(R.string.question_asia, true),
     };
     private int mCurrentIndex = 0;
+    private int mPoints = 0;
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+        savedInstanceState.putInt(KEY_POINTS, mPoints);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+
+        if(savedInstanceState != null){
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX,0);
+            mPoints = savedInstanceState.getInt(KEY_POINTS,0);
+        }
 
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
         mQuestionTextView.setOnClickListener(new View.OnClickListener() {
@@ -47,6 +69,8 @@ public class QuizActivity extends AppCompatActivity {
             public void onClick(View v) {
 
                 checkAnswer(true);
+                mTrueButton.setEnabled(false);
+                mFalseButton.setEnabled(false);
             }
         });
 
@@ -57,18 +81,39 @@ public class QuizActivity extends AppCompatActivity {
             public void onClick(View v) {
 
                 checkAnswer(false);
+                mTrueButton.setEnabled(false);
+                mFalseButton.setEnabled(false);
             }
         });
 
-        mNextBtton = (Button) findViewById(R.id.next_button);
-        mNextBtton.setOnClickListener(new View.OnClickListener() {
+        mNextButton = (ImageButton) findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+if( mCurrentIndex<mQuestionBank.length-1){
+
+    mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+    updateQuestion();
+}
+                if(mCurrentIndex == mQuestionBank.length-1){
+
+                    Toast.makeText(getApplicationContext(),"Odpowiedziałeś poprawnie na "+mPoints+ " pytań" , Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+        });
+
+        mPreviousButton = (ImageButton) findViewById(R.id.previous_button);
+        mPreviousButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                mCurrentIndex = (mCurrentIndex - 1) % mQuestionBank.length;
                 updateQuestion();
             }
         });
+
         updateQuestion();
 
     }
@@ -76,6 +121,11 @@ public class QuizActivity extends AppCompatActivity {
     private void updateQuestion() {
         int question = mQuestionBank[mCurrentIndex].getTextResId();
         mQuestionTextView.setText(question);
+
+        mTrueButton.setEnabled(true);
+        mFalseButton.setEnabled(true);
+
+
     }
 
     private void checkAnswer(boolean userPressedTrue) {
@@ -84,6 +134,7 @@ public class QuizActivity extends AppCompatActivity {
 
         if (userPressedTrue == answerIsTrue) {
             messageResId = R.string.correct_toast;
+            mPoints++;
         } else {
             messageResId = R.string.incorrect_toast;
         }
